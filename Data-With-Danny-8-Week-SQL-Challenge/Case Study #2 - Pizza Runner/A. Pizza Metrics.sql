ad0503910ed485622576cc9523afc04b62b46f7b
Serdar Sahbudak <61648255+serdarsahbudak@users.noreply.github.com>
2022-12-23 00:35:45 +0000

    Update A. Pizza Metrics.sql

--- Data-With-Danny-8-Week-SQL-Challenge/Case Study #2 - Pizza Runner/A. Pizza Metrics.sql
+++ Data-With-Danny-8-Week-SQL-Challenge/Case Study #2 - Pizza Runner/A. Pizza Metrics.sql
@@ -1,1 +1,14 @@
+--A. Pizza Metrics
+--1. How many pizzas were ordered?
+SELECT COUNT(order_id) as unique_orders
+FROM pizza_runner.customer_orders;
 
+--2. How many unique customer orders were made?
+SELECT COUNT(DISTINCT order_id) as orders
+FROM pizza_runner.customer_orders;
+
+--3. How many successful orders were delivered by each runner?
+SELECT runner_id, COUNT(order_id) as succesful_orders
+FROM pizza_runner.runner_orders r
+WHERE cancellation IS NULL
+GROUP BY runner_id;
