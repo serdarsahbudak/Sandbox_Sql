209d102e9222d3150e6be50fadb2831a41b5fd59
Serdar Sahbudak <61648255+serdarsahbudak@users.noreply.github.com>
2022-12-23 21:10:52 +0000

    Update Data Cleansing.sql

--- Data-With-Danny-8-Week-SQL-Challenge/Case Study #2 - Pizza Runner/Data Cleansing.sql
+++ Data-With-Danny-8-Week-SQL-Challenge/Case Study #2 - Pizza Runner/Data Cleansing.sql
@@ -9,48 +9,49 @@
 --duration - remove null and mins, and minutes
 --cancelation - remove null and replace ''
 
+DROP TABLE IF EXISTS runner_orders_temp;
 CREATE TEMP TABLE runner_orders_temp AS
 SELECT order_id, runner_id,
-CASE 
-	WHEN pickup_time LIKE 'null' THEN ' '
-	ELSE pickup_time END as pickup_time,
-CASE 	
-	WHEN distance LIKE 'null' THEN  ' '
+CAST(CASE 
+	WHEN pickup_time LIKE 'null' THEN NULL
+	ELSE pickup_time END AS TIMESTAMP) AS pickup_time,
+CAST(CASE 	
+	WHEN distance LIKE 'null' THEN NULL
+    	WHEN distance LIKE '' THEN NULL
     	WHEN distance LIKE '%km' THEN TRIM('km' from distance)
-    	ELSE distance END as distance,
-CASE
-	WHEN duration LIKE 'null' THEN ' '
-   	 WHEN duration LIKE '%mins' THEN TRIM('mins' from duration) 
+    	ELSE distance END AS DECIMAL(5,2)) AS distance,
+CAST(CASE
+	WHEN duration LIKE 'null' THEN NULL
+   	WHEN duration LIKE '%mins' THEN TRIM('mins' from duration) 
     	WHEN duration LIKE '%minute' THEN TRIM('minute' from duration)
     	WHEN duration LIKE '%minutes' THEN TRIM('minutes' from duration)
-    	ELSE duration END as duration,
+    	ELSE duration END AS INT) AS duration,
 CASE 
-	WHEN cancellation IS NULL OR cancellation LIKE 'null' THEN ' '
-    	ELSE cancellation END as cancellation
+	WHEN cancellation LIKE 'null' THEN NULL
+    	WHEN cancellation LIKE '' THEN NULL
+    	ELSE cancellation END AS cancellation
 FROM pizza_runner.runner_orders;
 
 SELECT * 
 FROM runner_orders_temp;
 
-ALTER TABLE runner_orders_temp
-ALTER COLUMN pickup_time TYPE TIMESTAMP,
-ALTER COLUMN distance TYPE FLOAT,
-ALTER COLUMN duration TYPE INTEGER;
 
 --TABLE: customer_orders
 
 --exclusions - remove nulls and replace ''
 --extras - remove nulls and replace ''
 
-
+DROP TABLE IF EXISTS customer_orders_temp;
 CREATE TEMP TABLE customer_orders_temp AS
-SELECT order_id, runner_id, pizza_id,
+SELECT order_id, customer_id, pizza_id,
 CASE 
-	WHEN exclusions IS NULL OR exclusions LIKE 'null' THEN ' '
-	ELSE exclusions END as exclusions,
+	WHEN exclusions LIKE 'null' THEN NULL
+    WHEN exclusions LIKE '' THEN NULL
+	ELSE exclusions END AS exclusions,
 CASE 	
-	WHEN extras IS NULL OR extras LIKE 'null' OR extras LIKE '%NaN' THEN  ' ' 
-    ELSE extras END as extras, order_time
+	WHEN extras LIKE 'null' THEN NULL
+    WHEN extras LIKE '' THEN NULL
+    ELSE extras END AS extras, order_time
 FROM pizza_runner.customer_orders;
 
 SELECT * 
